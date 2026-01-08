ff78876d78f778dba6ca1b7e7ab4238f1db357ff
Dilshod-22 <myprofileforgithub@gmail.com>
2026-01-08 23:39:45 +0500

    fixed some bags

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeeModule } from './employee/employee.module';
 import { PositionModule } from './position/position.module';
+import { DepartmentModule } from './department/department.module';
 
 @Module({
-  imports: [EmployeeModule, PositionModule],
+  imports: [EmployeeModule, PositionModule, DepartmentModule],
   controllers: [AppController],
   providers: [AppService],
 })
